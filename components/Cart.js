2ea7041bd651e42e6b3fe78bcb8734cfbf36a682
dungbduse151194 <dungbduse151194@fpt.edu.vn>
2023-07-09 19:19:49 +0700

    initial commit
    create 2 component: Cart & History

---
+++ components/Cart.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View } from "react-native";
+
+export const Cart = () => {
+  return <View>Cart</View>;
+};
---
+++ components/History.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const History = () => {
+  return <View>History</View>;
+};
+
+export default History;
